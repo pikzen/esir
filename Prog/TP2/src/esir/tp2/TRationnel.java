19fb094dc1b284593712d81f5686a74e96ca9db7
siharaProject <cocov2bis@hotmail.fr>
2014-10-21 19:43:56 +0200

    Ouais update de batard
    en fait ca lis et tout sa mère

--- Prog/TP2/src/esir/tp2/TRationnel.java
+++ Prog/TP2/src/esir/tp2/TRationnel.java
@@ -1,19 +1,57 @@
 package esir.tp2;
+package rationnel;
 
-import types.*;
-import rationnel.*;
+
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class TRationnel {
 	public static void main(String[] args) {
-		Rationnel r1 = new RationnelSimple(18, 5);
-		Rationnel r2 = new RationnelSimple(32, 8);
+		int num, den;
+		Scanner entree = new Scanner(System.in);
+		
+		System.out.println("Entrez le numérateur : \n");
+		num = entree.nextInt();
+		System.out.println("Entrez le dénominateur : \n");
+		den = entree.nextInt();
+		int cpt = 0;
+		
+		ArrayList<Rationnel> listRat = new ArrayList<Rationnel>();
 
-		System.out.println("Rationnel : " + r1.toString());
-		System.out.println("Somme : " + r1.somme(r2));
-		System.out.println("Inverse : " + r1.inverse());
-		System.out.println("Réel : " + r1.valeur());
-		System.out.println("CompareTo : " + r1.compareTo(r2));
-		System.out.println("Equals ? : " + r1.equals(r2));
+		
+		Rationnel r1 = new RationnelSimple(num, den);
+		
+		while(r1.getDenominateur() != 0  && r1.getNumerateur() != 0){
+			
+			listRat.add(cpt, r1);
+			System.out.println("Entrez le numérateur : \n");
+			num = entree.nextInt();
+			System.out.println("Entrez le dénominateur : \n");
+			den = entree.nextInt();
+			r1 = new RationnelSimple(num, den);
+			cpt++;
+			
+		}
 
+
+		for(int compteur = 0; compteur < listRat.size(); compteur++){
+		System.out.println("Rationnel : " + listRat.get(compteur));
+		if(compteur == 0){
+			System.out.println("Somme : 0");
+		}else{
+			System.out.println("Somme : " +listRat.get(compteur).somme(listRat.get(compteur-1)));
+		}
+		System.out.println("Réel : " + listRat.get(compteur).valeur());
+		System.out.println("Inverse : " + listRat.get(compteur).inverse());
+		if(compteur != 0){
+			
+		System.out.println("Compare to : "+listRat.get(compteur).compareTo(listRat.get(compteur-1)));
+		System.out.println("Egal : "+listRat.get(compteur).equals(listRat.get(compteur-1)));
+		}else{
+			System.out.println("Impossible de procéder au compare to, manque précédent\n\n");
+		}
+		
+			
+		}
 	}
 }
