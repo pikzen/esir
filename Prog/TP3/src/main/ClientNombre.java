105a6e34c98ca8408b17559771f9c1b30c56018b
siharaProject <cocov2bis@hotmail.fr>
2014-11-18 14:15:47 +0100

    MAJ ClientNombre calcul nombres premiers

--- Prog/TP3/src/main/ClientNombre.java
+++ Prog/TP3/src/main/ClientNombre.java
@@ -1,74 +1,101 @@
 package main;
 
+import java.util.Random;
 import java.util.Scanner;
+
 import tableau.Block;
 
 public class ClientNombre {
 
-
-	
-	public static int saisie(){
+	public static int saisie() {
 		int nombreSaisi;
 		Scanner sc = new Scanner(System.in);
 		return nombreSaisi = sc.nextInt();
 
 	}
 	
+	public static Block<Integer> setTableau(int n, Block<Integer> tableauRemp) {
+		
+		for (int cpt = 2; cpt <= n; cpt++) {
 
-public static boolean estPremier(int n, Block<Integer> tableauPrems) {
+			tableauRemp.set(cpt-2, cpt);
+		}
+		
+		return tableauRemp;
+	}
+
+	public static boolean estPremier(int n, Block<Integer> tableauPrems) {
+		
+		for (int cpt = 2; cpt < n; cpt++) {
+
+			tableauPrems.set(cpt-2, cpt);
+			
+		}
 		
 		if (n < 0) { /* Si nombre négatif pas de nombre premier */
 			return false;
 		}
-		
-		for(int compteur = 0; compteur <= tableauPrems.size(); compteur++){
-			if(n%tableauPrems.get(compteur) == 0){ /* si le chiffre est premier avec le contenu du tableau à l'indice : compteur */
-				return false; /* On retourne faux */ 		
+
+		for (int compteur = 0; compteur < tableauPrems.size(); compteur++) {
+			
+			if (n % tableauPrems.get(compteur) == 0) { 
+				
+				return false; /* On retourne faux */
 			}
 		}
 		return true;
-	}  
-	
-	
-	static int calculerNombrePremiers(int N, Block<Integer> tableauPremier)
-	{
+	}
+
+	static int calculerNombrePremiers(int N, Block<Integer> tableauPremier) {
 		int cpt = 0;
 		int g = 0;
-		
-		while (!tableauPremier.full() && (cpt <= N)){
-			
-			if (estPremier(cpt, tableauPremier)){
-			
+
+		while (!tableauPremier.full() && (cpt <= N)) {
+
+			if (estPremier(cpt, tableauPremier)) {
+
 				tableauPremier.set(g, cpt);
 				g++;
 			}
-			
+
 			cpt++;
 		}
-		
-		if (tableauPremier.full() && (cpt <= N)){
-			return cpt-1;	
-		}else{
+
+		if (tableauPremier.full() && (cpt <= N)) {
+			return cpt - 1;
+		} else {
 			return N;
 		}
 	}
+
+	public static void remplirHasard(int nb) {
+		Block<Integer> newTableau = new Block<Integer>(nb); /*
+															 * Tableau Block de
+															 * capacité nb
+															 */
+		Random r1 = new Random();
+
+		for (int i = 0; i < nb; i++) {
+
+			int nombre = (r1.nextInt(nb));
+			newTableau.set(i, nombre);
+
+		}
+	}
+
 	
-	
-	public static void remplirHasard(int nb){
-		Block<Integer> tableau = new Block<Integer>(nb);
+	public static void eliminerPresents(Block<Integer> tab1, Block<Integer> tab2) {
+
 		
 	}
+
 	
-	
-	public static void eliminerPresents(Block<Integer> tab1, Block<Integer> tab2){
-		
-	}
-	
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		Block<Integer> tab = new Block<Integer>(100);
 		System.out.println("Entrez un nombre N");
 		int N = saisie();
-		int resultat = calculerNombrePremiers(N, tab);
+		/*tab = setTableau(N, tab);*/
+		boolean resultat = estPremier(N, tab);
 		System.out.println(resultat);
 	}
 }
