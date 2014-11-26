8f37f4a724a76865beb12938110bc7ce0a46be86
Florian Eula <eula.florian@gmail.com>
2014-11-26 14:39:28 +0100

    Commented shit

--- Prog/TP3/src/main/NombresPremiers.java
+++ Prog/TP3/src/main/NombresPremiers.java
@@ -6,8 +6,14 @@ import java.util.Scanner;
 import tableau.Tableau;
 import tableau.Block;
 
+/**
+* Implémente les méthodes relatives aux nombres premiers et autres. Voir TP.
+*/
 public class NombresPremiers {
 
+	/**
+	* Récupère un nombre saisi par l'utilisateur
+	*/
 	public static int saisie() {
 		int nombreSaisi;
 		Scanner sc = new Scanner(System.in);
@@ -15,76 +21,106 @@ public class NombresPremiers {
 
 	}
 
-	public static Tableau<Integer> setTableau(int n, Tableau<Integer> tableauRemp) {
-
+	/**
+	* Remplit un tableau avec TOUS les nombres compris entre 2 et n
+	* @param n Valeur maximale à rentrer dans le tableau
+	* @param tableauRemp Le tableau dans lequel les valeurs seront inserées
+	* NOTE: Modifie <code>tableauRemp</code>
+	*/
+	public static void setTableau(int n, Tableau<Integer> tableauRemp) {
 		for (int cpt = 2; cpt <= n; cpt++) {
-
 			tableauRemp.push_back(cpt);
 		}
-
-		return tableauRemp;
 	}
 
+	/**
+	* Vérifie si <code>n</code> est un nombre premier en le cherchant dans
+	* <code>tableauRemp</code>.
+	* @param n Entier à vérifier
+	* @param tableauPrems Liste de nombres premiers
+	* @return <code>true</code> si <code>n</code> est premier,
+	*		  <code>false</code> sinon
+	*/
 	public static boolean estPremier(int n, Tableau<Integer> tableauPrems) {
 		if (n < 0) { /* Si nombre négatif pas de nombre premier */
 			return false;
 		}
 
+		// Itération sur tous les nombres présents dans tableauPrems
+		// NOTE: Puisque ce tableau est trié, on pourrait utiliser une recherche
+		// dichotomique
+		// On essaie de prouver que le nombre n'est pas premier et une fois que
+		// toutes les solutions sont épuisées, on sait qu'il est premier
+		// (ou bien qu'il n'est pas dans tableauPrems)
 		for (int compteur = 0; compteur < tableauPrems.size(); compteur++) {
 			int val = tableauPrems.get(compteur);
 			if (val == 0) {
 				return false;
 			}
-			
+
 			if (n % val == 0) {
-				return false; /* On retourne faux */
+				return false;
 			}
 		}
 		return true;
 	}
 
-	public static int calculerNombresPremiers(int N, Tableau<Integer> tableauPremier) {
+	/**
+	* Calcule les nombres premiers de 2 à <code>n</code> et les insere dans
+	* <code>tableauPremier</code>
+	* NOTE: modifie <code>tableauPremier</code>
+	* @param n Valeur maximale pour les nombres premiers
+	* @param tableauPremier Stockage des premiers
+	* @return Le nombre de premiers si on à réussi à remplir le tableau
+	*		  <code>n</code> sinon
+	*/
+	public static int calculerNombresPremiers(int n, Tableau<Integer> tableauPremier) {
 		int cpt = 2;
-		
-		while (!tableauPremier.full() && (cpt <= N)) {
-			
+
+		// Jusqu'a ce qu'on ait réussi à remplir le tableau ou bien qu'on
+		// atteigne n, on teste chaque nombre successivement
+		// Si il est premier, alors il ne sera pas dans tableauPremier et on
+		// pourra l'ajouter. Sinon, on passe au nombre suivant
+		while (!tableauPremier.full() && (cpt <= n)) {
 			if (estPremier(cpt, tableauPremier)) {
 				tableauPremier.push_back(cpt);
 			}
 			cpt++;
 		}
-		
-		System.out.println("Tableau is full lololol cpt = " + N);
-		
 
-		if (tableauPremier.full() && (cpt <= N)) {
+		if (tableauPremier.full() && (cpt <= n)) {
 			return cpt - 1;
 		} else {
-			return N;
+			return n;
 		}
 	}
 
-	public static Block<Integer> remplirHasard(int nb) {
-		Block<Integer> newTableau = new Block<Integer>(nb); /*
-															 * Tableau Block de
-															 * capacité nb
-															 */
+	/**
+	* Renvoie un tableau constitué de n nombres aléatoires
+	*/
+	public static Tableau<Integer> remplirHasard(int nb) {
+		Block<Integer> newTableau = new Block<Integer>(nb);
 		Random r1 = new Random();
 
+		// Génération et insertion
 		for (int i = 0; i < nb; i++) {
-
 			int nombre = (r1.nextInt(nb));
 			newTableau.push_back(nombre);
-
 		}
 
 		return newTableau;
 	}
 
+	/**
+	* Applique un tri par insertion sur <code>tab</code>
+	* NOTE: modifie <code>tab</code>
+	* @param tab Le tableau à trier
+	*/
 	private static void insertionSort(Tableau<Integer> tab) {
-		 // On itère sur les éléments non triès. (On considère que l'élément à l'index 0 est déjà trié et le sera lorsqu'on inserera les autres éléments)
+		 // On itère sur les éléments non triès. (On considère que l'élément à
+		 // l'index 0 est déjà trié et le sera lorsqu'on inserera les autres
+		 // éléments)
 		 // On trouve la position à laquelle inserer l'élément actuelle.
-
 		 for (int unsortedElements = 1; unsortedElements < tab.size(); unsortedElements++) {
 		 	int j = unsortedElements;
 		 	while (j > 0  && tab.get(j - 1) > tab.get(j)) {
@@ -96,16 +132,24 @@ public class NombresPremiers {
 		 }
 	}
 
-	private static int RechDichoIter(int check, Tableau<Integer> nums ) {
-	        int hi = nums.size() - 1;
-	        int lo = 0;
+	/**
+	* Applique une recherche dichotomique pour trouver <code>check</code>
+	* dans le tableau <code>nums</code>
+	* @param check Le nombre à chercher
+	* @param nums Le tableau dans lequel chercher
+	* @return -1 si l'élément n'est pas présent, ou son index dans
+	*		  <code>nums</code> si il l'est
+	*/
+	private static int rechercheDichotomique(int check, Tableau<Integer> nums ) {
+	        int high = nums.size() - 1;
+	        int low = 0;
 	        int guess = 0;
-	        while(hi >= lo){
-                guess = lo + ((hi - lo) / 2);
+	        while(high >= low){
+                guess = low + ((high - low) / 2);
                 if(nums.get(guess) > check){
-                    hi = guess - 1;
+                    guess = high - 1;
                 }else if(nums.get(guess) < check){
-                    lo = guess + 1;
+                    low = guess + 1;
                 }else{
                     return guess;
                 }
@@ -113,16 +157,22 @@ public class NombresPremiers {
 	        return -1;
       }
 
-
-
+    /**
+    * Elimine dans <code>tab1</code> les nombres communs avec <code>tab2</code>
+	* NOTE: Modifie <code>tab1</code>
+    * @param tab1 Tableau ou les nombres seront supprimés
+    * @param tab2 Tableau ou chercher les nombres
+    * @return le nombre de nombres supprimés
+    */
 	public static int eliminerPresents(Tableau<Integer> tab1, Tableau<Integer> tab2) {
 		Tableau<Integer> tabTmp = new Block<Integer>(tab1.size());
+		// On trie le tableau pour pouvoir appliquer une recherche dichotomique
 		insertionSort(tab2);
 		int elementsSupprimes = 0;
 
-		System.out.print(" ");
-		for (int i = 0; i < tab1.size(); i++) { /* parcourir le tab1, penser dichotomie itérative*/
-			if(RechDichoIter(tab1.get(i), tab2) == -1){ /* Si on ne trouve pas l'élément tab1 dans tab2*/
+		// On parcourt tout tab1 et pour chaque élément, on recherche dans tab2
+		for (int i = 0; i < tab1.size(); i++) {
+			if (rechercheDichotomique(tab1.get(i), tab2) == -1){
 				tabTmp.push_back(tab1.get(i));
 
 			}else{
@@ -141,6 +191,10 @@ public class NombresPremiers {
 
 	}
 
+	/**
+	* Affiche le tableau <code>t1</code> sous la forme [x y z ...]
+	* @param t1 Le tableau à afficher
+	*/
 	private static void printTab(Tableau<Integer> t1) {
 		System.out.print("[");
 		for (int i = 0; i < t1.size(); i++) {
--- Prog/TP3/src/tableau/TableauBlock.java
+++ Prog/TP3/src/tableau/TableauBlock.java
@@ -2,7 +2,9 @@ package tableau;
 
 import types.Array;
 /**
-* Tableau redimensionnable, implémenté via un Array<T>
+* Tableau redimensionnable
+* Ce tableau se redimensionnera automatiquement lorsque sa taille maximale est
+* atteinte, en augmentant sa capacité de <code>BlockSize</code>
 */
 public class TableauBlock<T> implements Tableau<T> {
 	/**
@@ -20,30 +22,55 @@ public class TableauBlock<T> implements Tableau<T> {
 	*/
 	private Array<Block<T>> Elements;
 
+	/**
+	* Taille d'un block du tableau
+	*/
 	private int BlockSize;
 
+	/**
+	* Trouve l'index du block dans lequel sera présent l'élément à l'index
+	* <code>idx</code>
+	* @param idx Index de l'élément
+	*/
 	private int findBlock(int idx) {
+		// Pour les opérations get, set, push_back et pop_back, cet index
+		// sera nécessaire. Plutot que de réécrire la formule à chaque fois,
+		// findBlock() nous permettra de la réutiliser facilement
 		return idx / BlockSize;
 	}
 
+	/**
+	* Trouve l'index dans le Block ou sera présent l'élément à l'index
+	* <code>idx</code>
+	* @param idx Index de l'élément
+	*/
 	private int findPos(int idx) {
+		// Même raisons que findBlock(), simplifie l'écriture des algorithmes
+		// Dans ce cas, on soustrait juste à l'index l'index minimal de son bloc
 		return idx - (BlockSize * findBlock(idx));
 	}
 
+	/**
+	* Construit un nouveau tableau de capacité <code>maxSize</code>
+	* Les Block le composant ont une capacité de 128 éléments
+	*/
 	public TableauBlock(int maxSize) {
 		this(maxSize, 128);
 	}
 
 	/**
-	* Construit un nouveau tableau de capacité maximale <code>capacite</code>
+	* Construit un nouveau tableau de capacité maximale <code>maxSize</code> et
+	* de capacité de block <code>capaciteBlock</code>
 	* @param capacite Capacité maximale du tableau
 	*/
 	public TableauBlock(int maxSize, int capaciteBlock) {
-			assert (maxSize > 0) : "MaxSize should be > 0, MaxSize is :" + maxSize;
+			assert (maxSize > 0);
 
 			BlockSize = capaciteBlock;
 			MaximumSize = maxSize;
 
+			// Initialisation des Blocks nécessaires pour contenir maxSize
+			// éléments.
 			Elements = new Array(findBlock(MaximumSize) + 1);
 			for (int i = 0; i < Elements.length(); i++) {
 				Elements.set(i, new Block<T>(BlockSize));
@@ -81,7 +108,7 @@ public class TableauBlock<T> implements Tableau<T> {
 	*/
 	public T get(int i) {
 		assert i >= 0 && i < this.Size;
-		
+
 		return this.Elements.get(findBlock(i)).get(findPos(i));
 	}
 
@@ -92,19 +119,22 @@ public class TableauBlock<T> implements Tableau<T> {
 	* @param elem Nouvelle valeur de l'élément
 	*/
 	public void set(int i, T elem) {
-		assert i >= 0 && i < this.size() : "i is " + i + ", size is " + this.size();
+		assert i >= 0 && i < this.size();
 
 		this.Elements.get(findBlock(i)).set(findPos(i), elem);
 	}
 
 	/**
-	* Ajoute un élément en fin de tableau
+	* Ajoute un élément en fin de tableau, et redimensionne le tableau si
+	* nécéssaire
 	* @param elem Element à ajouter
 	* @pre !this.full()
 	*/
 	public void push_back(T elem) {
 		assert !this.full();
 
+		// On ajoute l'élément puis on redimensionne si besoin
+		// Ainsi le tableau sera toujours disponible
 		this.Elements.get(findBlock(Size)).push_back(elem);
 		this.Size++;
 		if (this.full()) {
@@ -125,10 +155,14 @@ public class TableauBlock<T> implements Tableau<T> {
 	/**
 	* Redimensionne le tableau stockant les données
 	* Rajoute un block à la fin
+	* NOTE: Ne réduit pas le nombre de blocks. Si le nombre de blocks est plus
+	* grand que le nombre de blocks nécessaire, ils ne seront pas supprimés
 	*/
 	private void resize() {
 		this.MaximumSize += BlockSize;
 
+		// Création d'un nouvel Array, on remplit avec les données actuelles
+		// Puis on ajoute un Block vide à la fin
 		Array<Block<T>> newArray = new Array<Block<T>>(Elements.length() + 1);
 		for (int i = 0; i < Elements.length(); i++) {
 			newArray.set(i, Elements.get(i));
