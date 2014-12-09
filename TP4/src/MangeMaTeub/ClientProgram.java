d0966867ca978442c83a0b711aba15c9b4be1160
Corentin Houchard <13004636@e008m09.istic.univ-rennes1.fr>
2014-12-09 15:06:25 +0100

    Commit ClientProgram

---
+++ TP4/src/MangeMaTeub/ClientProgram.java
@@ -0,0 +1,42 @@
+package MangeMaTeub;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class ClientProgram {
+
+	
+	public static String Saisie(){
+		Scanner entree = new Scanner(System.in);
+		System.out.println("Veuillez entrer le nom du fichier de données\n");
+		String fichier = entree.nextLine();
+		return fichier;
+	}
+	
+	public static void readFile() throws IOException{
+		String file = Saisie();
+		int nbLignes = 0;
+		InputStream ips = new FileInputStream(file);
+		InputStreamReader ipsr=new InputStreamReader(ips);
+		BufferedReader br=new BufferedReader(ipsr);
+		String ligne;
+
+		while((ligne=br.readLine())!=null){
+			System.out.println(ligne);
+			nbLignes++;
+		}
+		br.close();
+	}
+	
+	public static int[] initTab(int nombre){
+		int[] tableau = new int[nombre];
+		
+		return tableau;
+	}
+	
+}
