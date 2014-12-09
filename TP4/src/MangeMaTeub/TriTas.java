9a7048a4fe92526100e2b2ba5c157a32aba638dd
Corentin Houchard <13004636@e008m09.istic.univ-rennes1.fr>
2014-12-09 14:43:23 +0100

    Commit init

---
+++ TP4/src/MangeMaTeub/TriTas.java
@@ -0,0 +1,20 @@
+package MangeMaTeub;
+
+public class TriTas {
+	
+	private int[] tnb;
+	int p, nb;
+
+	static void ajouterTas(int [] tnb, int p) {
+		
+	}
+	
+	static void supprimerMax(int [] tnb, int p) {
+		
+	}
+	
+	public static void trier(int [] tnb, int nb) {
+		
+	}
+	
+}
