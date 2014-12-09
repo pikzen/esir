b0144f6dc5a6ed78a233de08aca2b09a8e689728
Corentin Houchard <cocov2bis@hotmail.fr>
2014-12-09 15:10:24 +0100

    Commit TriTas

--- TP4/src/MangeMaTeub/TriTas.java
+++ TP4/src/MangeMaTeub/TriTas.java
@@ -6,15 +6,16 @@ public class TriTas {
 	int p, nb;
 
 	static void ajouterTas(int [] tnb, int p) {
+		assert (p >=1 && p < tnb.length);
 		
 	}
 	
 	static void supprimerMax(int [] tnb, int p) {
-		
+		assert (p >=1 && p < tnb.length);
 	}
 	
 	public static void trier(int [] tnb, int nb) {
-		
+		assert (nb >= 1 && nb < tnb.length);
 	}
 	
 }
