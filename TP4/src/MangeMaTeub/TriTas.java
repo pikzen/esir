701599239a3acb3bf61809dbda66c76f8550ed64
Corentin Houchard <cocov2bis@hotmail.fr>
2014-12-09 15:46:32 +0100

    Fonction TriTas

--- TP4/src/MangeMaTeub/TriTas.java
+++ TP4/src/MangeMaTeub/TriTas.java
@@ -7,7 +7,17 @@ public class TriTas {
 
 	static void ajouterTas(int [] tnb, int p) {
 		assert (p >=1 && p < tnb.length);
+		/* Quand on insère, tant que p >= 1 et que le fils est plus grand on monte */
 		
+		int indicePere = (p-1)/2;
+		
+		while(p >= 1 && tnb[indicePere]<tnb[p]){ /* Tant que l'indice du pere est < à l'indice de p*/
+		
+			int temp = tnb[p]; /* Wllh j'crois que ça marche */
+			tnb[p] = tnb [indicePere]; 
+			tnb[indicePere] = temp;
+			p = indicePere;
+		}
 	}
 	
 	static void supprimerMax(int [] tnb, int p) {
@@ -17,5 +27,7 @@ public class TriTas {
 	public static void trier(int [] tnb, int nb) {
 		assert (nb >= 1 && nb < tnb.length);
 	}
+	
+	
 	
 }
