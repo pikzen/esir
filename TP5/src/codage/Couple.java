0c48ba5147b8122324dfaf6da27bcec4efbb5b3b
Corentin Houchard <cocov2bis@hotmail.fr>
2015-01-06 14:03:15 +0100

    Le TP5 sa mère le cheesy crust

---
+++ TP5/src/codage/Couple.java
@@ -0,0 +1,27 @@
+package codage;
+
+/**
+ * Représente un couple de deux éléments de type quelconque. 
+ */
+
+public class Couple<T1, T2> { 
+	private T1 First;
+	private T2 Second;
+
+	public Couple(T1 first, T2 second) {
+		this.First = first;
+		this.Second = second;
+	}
+
+// ===================================================================
+// GETTERS
+// ===================================================================
+	
+	public T1 getFirst() {
+		return this.First;
+	}
+
+	public T2 getSecond() {
+		return this.Second;
+	}
+}
